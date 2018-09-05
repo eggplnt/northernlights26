b535f68451b641207ca42c3d5e538be9a2c7dacc
eggplnt <tohru.t.suzuki@gmail.com>
2018-09-05 19:20:59 +0900

    forst commit

--- northernlights/penguin/views.py
+++ northernlights/penguin/views.py
@@ -5,9 +5,17 @@ def index(request):
     return render(request, 'index.html')
 
 def exercise(request):
+    text = 'テストです。'
+    try:
+        input_text = request.POST['input_text']
+        print(input_text)
+        text = input_text
+    except:
+        return render(request, 'exercise.html')
+
     now = datetime.now();
     context = {
-        'text': 'テストです。',
+        'text': text,
         'time': now,
     }
     return render(request, 'exercise.html', context)
