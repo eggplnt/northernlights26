fa6234ea32b4cdbe407d0df032484b76cb116c70
eggplnt <tohru.t.suzuki@gmail.com>
2018-09-05 18:41:27 +0900

    first commit

--- northernlights/penguin/views.py
+++ northernlights/penguin/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from datetime import datetime
 
 def index(request):
     return render(request, 'index.html')
 
-def test(request):
-    return render(request, 'test.html')
+def exercise(request):
+    now = datetime.now();
+    context = {
+        'text': 'テストです。',
+        'time': now,
+    }
+    return render(request, 'exercise.html', context)
